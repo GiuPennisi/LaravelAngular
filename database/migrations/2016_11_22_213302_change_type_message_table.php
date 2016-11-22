d2ce6ab9ced6a6bfb6b84f8ee53e3a067ae7b6cb
DESKTOP-R1148KV\AlanB <applelan>
2016-11-22 19:08:52 -0300

    New migration to ch ange mgsSendDate type to timestamp

---
+++ database/migrations/2016_11_22_213302_change_type_message_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeTypeMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('message', function ($table) {
+            $table->timestamp('mgsSenddate')->change();
+});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('message',function ($table){
+            $table->date('mgsSenddate');
+        });
+    }
+}
